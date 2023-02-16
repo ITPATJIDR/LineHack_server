612ffe4234d9d2e66fd66b81d94b0b43fdb15c5b
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-16 21:32:56 +0700

    add cors config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors")
 
 dotenv.config()
 
-app.use(cors({ origin: ["http://localhost:3000","https://line-hack-client.vercel.app"] }))
+app.use(cors({ origin: ["http://localhost:3000","https://line-hack-client.vercel.app"], credentials: true }))
 app.use(express.json())
 
 app.use("/user", require("./routers/userRouter"))
@@ -17,6 +17,18 @@ app.use("/camp", require("./routers/campRouter"))
 app.get("/", (req, res) => {
     res.send("hi")
 })
+
+app.use(function(req,res,next) {
+res.header('Access-Control-Allow-Credentials', 'true');
+res.header('Access-Control-Allow-Origin', req.headers.origin);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+if ('OPTIONS' == req.method) {
+     res.send(200);
+ } else {
+     next();
+ }
+});
 
 
 app.listen(port, () => {
