43bba12ef7c090201b8a0129956b09f92ea52d2e
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-18 21:38:05 +0700

    fix create service

--- controllers/serviceControllers.js
+++ controllers/serviceControllers.js
@@ -13,6 +13,7 @@ const serviceCtrl = {
 					serviceDistance: serviceDistance    
 				}
 			})
+			res.status(200).json({message:"add service Success!!!"})
 		}catch (err){
 			res.status(500).json({message: err.message});
 		}
