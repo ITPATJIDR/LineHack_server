2a9e0e4b37b6f6e7dfab04a50501f83ad4d20466
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-18 21:32:25 +0700

    add service Ctrl

--- controllers/serviceControllers.js
+++ controllers/serviceControllers.js
@@ -1,5 +1,30 @@
+const prisma = require('../utils/prisma');
 const serviceCtrl = {
-
+	createService: async (req,res,next) => {
+		try{
+			const {serviceImage,serviceName,serviceDescription, serviceContact, serviceLineContact,serviceDistance} = req.body
+			await prisma.servive.create({
+				data:{
+					serviceImage: serviceImage,
+					serviceName: serviceName,
+					serviceDescription: serviceDescription,
+					serviceContact: serviceContact,
+					serviceLineContact: serviceLineContact,
+					serviceDistance: serviceDistance    
+				}
+			})
+		}catch (err){
+			res.status(500).json({message: err.message});
+		}
+	},
+	getAllService: async (req,res,next) => {
+		try{
+			const result = await prisma.servive.findMany()
+			res.status(200).json({data: result});
+		}catch (err){
+			res.status(500).json({message: err.message});
+		}
+	} 
 }
 
 module.exports = serviceCtrl
--- routers/serviceRouter.js
+++ routers/serviceRouter.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const serviceCtrl = require('../controllers/serviceControllers')
 
+router.post("/createService", serviceCtrl.createService)
+router.get("/getAllService", serviceCtrl.getAllService)
 
 module.exports = router
