b0bba66804bf778d21d467e7a8170549001651fd
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-18 20:32:18 +0700

    add get facility

--- routers/campRouter.js
+++ routers/campRouter.js
@@ -5,5 +5,6 @@ router.post("/addCamp",campCtrl.addCamp);
 router.get("/getAllCamp",campCtrl.getAllCamp);
 router.post("/getCampbyId",campCtrl.getCampById);
 router.post("/addCampFacility",campCtrl.addCampFacilityDescription);
+router.get("/getCampFacility",campCtrl.getCampFacilityDescription);
 
 module.exports = router
