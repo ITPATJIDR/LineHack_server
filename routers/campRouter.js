e320c9398c5e440ff99990814a26ca3790fe88b8
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-20 19:55:39 +0700

    fix db

--- routers/campRouter.js
+++ routers/campRouter.js
@@ -4,7 +4,6 @@ const campCtrl = require('../controllers/campControllers')
 router.post("/addCamp",campCtrl.addCamp);
 router.get("/getAllCamp",campCtrl.getAllCamp);
 router.post("/getCampbyId",campCtrl.getCampById);
-router.post("/addCampFacility",campCtrl.addCampFacilityDescription);
 router.post("/booking",campCtrl.booking);
 
 module.exports = router
