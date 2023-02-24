d514a2c7585ec552f3b80d98cea7cb65c1992a43
ITPATJIDR <ittipatjitrada@gmail.com>
2023-02-25 00:58:55 +0700

    test

--- routers/serviceRouter.js
+++ routers/serviceRouter.js
@@ -3,5 +3,6 @@ const serviceCtrl = require('../controllers/serviceControllers')
 
 router.post("/createService", serviceCtrl.createService)
 router.get("/getAllService", serviceCtrl.getAllService)
+router.post("/getServiceById", serviceCtrl.getServiecById)
 
 module.exports = router
